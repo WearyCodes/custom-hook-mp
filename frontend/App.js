204940e57fa8576058b674e81a08f3aaa0d8ce91
Samuel Sanchez <wearycodes@gmail.com>
2023-11-15 19:55:52 -0600

    Custom Hook MP

--- frontend/App.js
+++ frontend/App.js
@@ -4,9 +4,12 @@ import axios from "axios";
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
+import { useDarkMode } from "./components/useDarkMode";
+
+
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
 
   useEffect(() => {
     axios
